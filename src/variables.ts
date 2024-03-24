aba6ebdad294fdf35c647bba4131f0edf942ff6e
kiyotd <torigoedesign@gmail.com>
2024-03-24 16:27:31 +0900

    v1.3.6
    feat(variables.ts): 上下のマージン・パディングを commonScopes から yScopes に変更
    feat(variables.ts): コメント追加

--- src/variables.ts
+++ src/variables.ts
@@ -4,6 +4,19 @@ export const OutputScssDefaultPath = "../scss/default";
 export const OutputScssBreakPointPath = "../scss/breakpoint";
 export const importRfsStr = '@import "../../node_modules/rfs/scss";\n\n';
 
+// 10px = rfs-0-625
+// 12px = rfs-0-75
+// 14px = rfs-0-875
+// 16px = rfs-1
+// 18px = rfs-1-125
+// 20px = rfs-1-25
+// 24px = rfs-1-5
+// 28px = rfs-1-75
+// 32px = rfs-2
+// 36px = rfs-2-25
+// 40px = rfs-2-5
+// 48px = rfs-3
+
 /**
  * rfsScopesは、rfsに関連するCSSクラスを生成するための範囲とステップを定義した配列です。
  */
@@ -25,20 +38,30 @@ export const commonScopes: Scope[] = [
 ];
 
 /**
+ * yScopesは、上下のマージンやパディングを生成するための範囲とステップを定義した配列です。
+ */
+export const yScopes: Scope[] = [
+  { startNum: 0, endNum: 4, step: 0.125 },
+  { startNum: 4, endNum: 8, step: 0.25 },
+  { startNum: 8, endNum: 12, step: 0.5 },
+  { startNum: 12, endNum: 48, step: 1 }
+];
+
+/**
  * entriesは、生成するCSSクラスの設定を定義したEntryオブジェクトの配列です。
  */
 export const entries: Entry[] = [
   { name: "rfs", scopes: rfsScopes, props: ["font-size"] },
-  { name: "rmt", scopes: commonScopes, props: ["margin-top"] },
-  { name: "rmb", scopes: commonScopes, props: ["margin-bottom"] },
+  { name: "rmt", scopes: yScopes, props: ["margin-top"] },
+  { name: "rmb", scopes: yScopes, props: ["margin-bottom"] },
   { name: "rml", scopes: commonScopes, props: ["margin-left"] },
   { name: "rmr", scopes: commonScopes, props: ["margin-right"] },
   { name: "rmx", scopes: commonScopes, props: ["margin-left", "margin-right"] },
-  { name: "rmy", scopes: commonScopes, props: ["margin-top", "margin-bottom"] },
-  { name: "rpt", scopes: commonScopes, props: ["padding-top"] },
-  { name: "rpb", scopes: commonScopes, props: ["padding-bottom"] },
+  { name: "rmy", scopes: yScopes, props: ["margin-top", "margin-bottom"] },
+  { name: "rpt", scopes: yScopes, props: ["padding-top"] },
+  { name: "rpb", scopes: yScopes, props: ["padding-bottom"] },
   { name: "rpl", scopes: commonScopes, props: ["padding-left"] },
   { name: "rpr", scopes: commonScopes, props: ["padding-right"] },
   { name: "rpx", scopes: commonScopes, props: ["padding-left", "padding-right"] },
-  { name: "rpy", scopes: commonScopes, props: ["padding-top", "padding-bottom"] },
+  { name: "rpy", scopes: yScopes, props: ["padding-top", "padding-bottom"] },
 ];
